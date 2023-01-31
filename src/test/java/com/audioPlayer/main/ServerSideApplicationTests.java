5fc8cc3f61f09962b104cea63289731e97a7b435
mohajon33 <mohajon33@gmail.com>
2023-01-31 12:55:38 +0600

    create audio player server structure

---
+++ src/test/java/com/audioPlayer/main/ServerSideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.audioPlayer.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerSideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
