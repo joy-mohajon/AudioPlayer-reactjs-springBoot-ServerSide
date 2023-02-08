b03a220d8ef34ed679f5308dc2697b10b0c78ef3
mohajon33 <mohajon33@gmail.com>
2023-02-08 12:28:03 +0600

    implement getAudiosByLanguage controller

--- src/main/java/com/audioPlayer/main/repository/AudioDetailsRepo.java
+++ src/main/java/com/audioPlayer/main/repository/AudioDetailsRepo.java
@@ -1,11 +1,17 @@
 package com.audioPlayer.main.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.audioPlayer.main.model.AudioDetails;
 
 @Repository
 public interface AudioDetailsRepo extends JpaRepository<AudioDetails, Integer>{
+
+	@Query("from AudioDetails where language = ?1")
+	List<AudioDetails> getAudiosByLang(String lang);
 
 }
